6b07eaf1a9701e4c7e9232cd91cec979a173d5a1
raymgilian <beformax@gmail.com>
2017-12-06 14:31:02 +0200

    test1

---
+++ src/test/java/constants/DefaultNewAgentConstants.java
@@ -0,0 +1,25 @@
+package constants;
+
+/**
+ * Class contains create agent constants
+ */
+public final class DefaultNewAgentConstants
+{
+    //Advantageu new agent data
+    public static final String EMAIL_ENDING = "@ukr.net";
+    public static final String FIRST_NAME = "Auto";
+    public static final String LAST_NAME = "Test";
+    public static final String DEFAULT_PASSWORD = "qwerty";
+    public static final String LICENSE_NUMBER = "666666666";
+    public static final String PHONE_NUMBER = "(012)345-6789";
+    public static final String EDUCATION = "Ordo Templi Orientis";
+    public static final String COMPANY = "Malleus Maleficarum";
+    public static final String WEBSITE = "uk.wikipedia.org";
+    public static final String BIO = "A common modern version of the omnipotence paradox is expressed in the question: \"Can [an omnipotent being] create a stone so heavy that it cannot lift it?\" This question generates a dilemma. The being can either create a stone it cannot lift, or it cannot create a stone it cannot lift. If the being can create a stone that it cannot lift, then it seems that it can cease to be omnipotent. If the being cannot create a stone it cannot lift, then it seems it is already not omnipotent.";
+
+
+    private DefaultNewAgentConstants()
+    {
+        //nothing. Constant archive class.
+    }
+}

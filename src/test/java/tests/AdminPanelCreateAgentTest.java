2cbbe73ad29fa9b7bfc7fb13eef7e2795eb88eb1
artemkalantai <shmublon@gmail.com>
2017-12-07 16:49:22 +0100

    all examples

--- src/test/java/tests/AdminPanelCreateAgentTest.java
+++ src/test/java/tests/AdminPanelCreateAgentTest.java
@@ -3,7 +3,10 @@ package tests;
 import beans.Agent;
 import constants.Constants;
 import core.BaseTest;
+import io.qameta.allure.*;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import pages.AdminLoginPage;
 import pages.AgentsPage;
 import pages.CreateAgentPage;
@@ -15,16 +18,36 @@ import static com.codeborne.selenide.Selenide.open;
 public class AdminPanelCreateAgentTest extends BaseTest{
 
     @Test
+    @DisplayName("Fucking allure test")
+    @Description("Some fucking description")
+    @Issue("JIRA-123")
+    @TmsLink("jira-link")
+    @Severity(SeverityLevel.CRITICAL)
+    @Category(RegressionTest.NewTest.class)
     public void adminCreateNewAgent() {
         Agent expectedAgent = Agent.getFromXml("src/main/resources/agent.xml");
         String email = "autotest" + Long.toString(new Random().nextLong()) + expectedAgent.getEmail();
         expectedAgent.setEmail(email);
-        AdminLoginPage adminLoginPage = open(Constants.SITE_AU_ADRESS
-                + Constants.SITE_AU_ADMIN_SUBADRESS, AdminLoginPage.class);
-        AgentsPage agentsPage = adminLoginPage.login(Constants.SITE_AU_ADMIN_LOGIN, Constants.SITE_AU_ADMIN_PASSWORD);
-        CreateAgentPage createAgentPage = agentsPage.goToCreateAgentPage();
-        createAgentPage.createAgent(expectedAgent);
-        createAgentPage.verifyCreatedAgent(expectedAgent);
+        AgentsPage agentsPage = login();
+        CreateAgentPage createAgentPage = createAgent(agentsPage, expectedAgent);
+        verifyAgentData(createAgentPage, expectedAgent);
     }
 
+    @Step
+    public AgentsPage login() {
+        AdminLoginPage adminLoginPage = open(Constants.SITE_AU_ADRESS
+                + Constants.SITE_AU_ADMIN_SUBADRESS, AdminLoginPage.class);
+        return adminLoginPage.login(Constants.SITE_AU_ADMIN_LOGIN, Constants.SITE_AU_ADMIN_PASSWORD);
+    }
+
+    @Step
+    public CreateAgentPage createAgent(AgentsPage agentsPage, Agent agent) {
+        CreateAgentPage createAgentPage = agentsPage.goToCreateAgentPage();
+        return createAgentPage.createAgent(agent);
+    }
+
+    @Step
+    public void verifyAgentData(CreateAgentPage createAgentPage, Agent expectedAgent) {
+        createAgentPage.verifyCreatedAgent(expectedAgent);
+    }
 }
